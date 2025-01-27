1e561719c180fd1620c03b8101e197b0a4ff9e2d
MatheusHenriqueNF <matheus.henrique26@unifesp.br>
2025-01-27 15:46:03 -0300

    Aula 1 | aula inicial

--- 1-meuPrimeiroPrograma.js
+++ 1-meuPrimeiroPrograma.js
@@ -1,1 +1,11 @@
-console.log("meu primeiro programa");
+console.log("meu primeiro programa: Trabalhando com variaveis");
+
+const idade = 26;
+
+console.log(idade);
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade/2);
+
+const idadeSomada = idade + 2;
+console.log(idadeSomada);
