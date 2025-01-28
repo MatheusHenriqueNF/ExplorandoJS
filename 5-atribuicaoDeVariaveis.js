cd6a838ea31b349a944e0a29a9c1511e88230bbd
MatheusHenriqueNF <matheus.henrique26@unifesp.br>
2025-01-28 15:47:30 -0300

    aula 5 | atribuindo variaveis

---
+++ 5-atribuicaoDeVariaveis.js
@@ -0,0 +1,17 @@
+console.log("Trabalhando com atribuição de variáveis");
+
+const idade = 29;
+const primeironome = "Ricardo";
+// let nome = "Ricardo";
+const sobrenome = "Burgan";
+
+console.log(primeironome + " " + sobrenome);
+console.log(primeironome, sobrenome);
+console.log(`Meu nome é ${primeironome} ${sobrenome}` );
+
+let contador = 0;
+contador = contador + 1;
+
+// nome = nome + sobrenome;
+const nomeCompleto = primeironome + sobrenome;
+console.log(nomeCompleto);
