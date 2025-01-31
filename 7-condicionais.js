63fa3629e0b54127d6f7e6d9d619b7ca6a766a69
MatheusHenriqueNF <matheus.henrique26@unifesp.br>
2025-01-31 15:48:47 -0300

    Aula 9 | operadores conditional

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -6,24 +6,36 @@ const listaDeDestinos = new Array(
     `Rio de Janeiro`
 );
 
-const idadeComprador = 15;
+const idadeComprador = 19;
 const estaAcampanhada = true;
+const temPassagemComprada = true;
 
 console.log("Destinos possíveis:");
 console.log(listaDeDestinos);
 
-if(idadeComprador >= 18){
-    console.log("Comprador é maior de idade");
-    listaDeDestinos.splice(1,1); //removendo item
-}
-else if(estaAcampanhada == true){
-        console.log("Comprador está acampanhado");
-        listaDeDestinos.splice(1,1); //removendo item
-    } else{
-    
-        console.log("Comprador não é maior de idade e não posso vender");
+// if(idadeComprador >= 18 ){
+//     console.log("Comprador é maior de idade");
+//     listaDeDestinos.splice(1,1); //removendo item
+// } else if(estaAcampanhada == true){
+//         console.log("Comprador está acampanhado");
+//         listaDeDestinos.splice(1,1); //removendo item
+// } else{
+//     console.log("Comprador não é maior de idade e não posso vender");
+//     }
 
-        }
+if(idadeComprador >= 18 || estaAcampanhada == true){
+    console.log("Boa Viagem!");
+    listaDeDestinos.splice(1,1); //removendo item
+} else{
+    console.log("Comprador não é maior de idade e não posso vender");
+}
+
+console.log("Embarque: \n \n");
+if(idadeComprador >= 18 && temPassagemComprada == true){
+    console.log("Boa Viagem!");
+} else{
+    console.log("Você não pode embarcar");
+}
 
 console.log(listaDeDestinos);
 
